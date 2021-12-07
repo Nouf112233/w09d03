b7b3c2a58e5869a5789b0d0c6ee12630d629b331
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-12-07 18:41:52 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Account from "./components/Account";
+import UserTask from "./components/UserTask";
+import AdminTask from "./components/AdminTask";
+import { useSelector } from "react-redux";
 
 
 
 function App() {
+
+  const state = useSelector((state) => {
+    console.log("state", state);
+    return state;
+  });
  
   return (
     <>
+    <p>hello</p>
     </>
    
   );
---
+++ src/components/AdminTask/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminTask() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AdminTask
